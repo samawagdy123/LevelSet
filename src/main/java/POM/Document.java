7f3d82223f210d6d5239b4fb4f40457bcb28f9c3
sama wagdy <60358893+samawagdy123@users.noreply.github.com>
2024-03-20 16:44:19 +0200

    Update Document.java

--- src/main/java/POM/Document.java
+++ src/main/java/POM/Document.java
@@ -5,40 +5,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class Document {
     WebDriver driver;
-    //HomePage homePage;
     WebDriverWait wait;
-    By docNameExampleElement = new By.ByXPath("//div[@class=\"left\" and contains(text(), \"File a Lien\")]");
-    WebElement docName=driver.findElement(docNameExampleElement);
-    By docPriceExampleElement = new By.ByXPath("//span[@class=\"price-amount\" and contains(text(), \"449\")]");
-    WebElement docPrice=driver.findElement(docPriceExampleElement);
+    By docNameExampleElement = new By.ByXPath("//div[@class='left' and contains(text(), 'File a Lien')]");
+    By docPriceExampleElement = new By.ByXPath("//span[@class='price-amount' and contains(text(), '449')]");
 
     public Document(WebDriver driver){
         this.driver= driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        //homePage=new HomePage(driver);
     }
+
     public String getDocName(){
+        WebElement docName=driver.findElement(docNameExampleElement);
         return docName.getText();
     }
+
     public String getDocPrice(){
+        WebElement docPrice=driver.findElement(docPriceExampleElement);
         return docPrice.getText();
     }
+
     public void navigateToDocPage(){
         driver.navigate().refresh ();
         wait.until(ExpectedConditions.visibilityOfElementLocated(docNameExampleElement));
     }
 
-    public void docPageLoaded(){
-        //homePage.getPaidClick();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(docNameExampleElement));
-
-        if(!docName.isDisplayed()) {
+    public void checkDocPageLoaded(){
+       wait.until(ExpectedConditions.visibilityOfElementLocated(docNameExampleElement));
+       WebElement docName=driver.findElement(docNameExampleElement);
+       if(!docName.isDisplayed()) {
             navigateToDocPage();
-        }
+       }
     }
 }
